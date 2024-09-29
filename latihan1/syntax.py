a01d659fd863f9aa0b62b4da84c488bb48c3ad67
Efend1 <kujanglarang31@gmail.com>
2024-09-29 17:56:40 +0700

    mulai sedikit belajar python

---
+++ latihan1/syntax.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print("Memulai belajar scripting dengan python")
+
+nama = "Efendi"
+print("Perkenalkan saya "+nama)
+
+if nama == 'Efendi':
+    print("Selamat datang "+nama)
---
+++ latihan1/variabel-tipeData.py
@@ -0,0 +1,18 @@
+#variabel
+nama_variabel = "ini adalah nilainya"
+# namaVariabel
+#_nama
+#nama
+#nama2
+#
+#
+#
+
+#tipe data
+nama_ku = "Efendi"
+umur = 20
+tinggi = 171.1
+
+print (nama_ku); print (type(nama_ku))
+print (umur); print (type(umur))
+print (tinggi); print (type(tinggi))
