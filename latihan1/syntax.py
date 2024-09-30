423b1ba56071c8550f48ab3e62859eeceda25039
Efend1 <kujanglarang31@gmail.com>
2024-09-30 16:05:30 +0700

    day 2

--- latihan1/syntax.py
+++ latihan1/syntax.py
@@ -4,5 +4,21 @@ print("Memulai belajar scripting dengan python")
 nama = "Efendi"
 print("Perkenalkan saya "+nama)
 
-if nama == 'Efendi':
-    print("Selamat datang "+nama)
+if nama == 'Efendi': print("Selamat datang "+nama)
+
+
+
+kalimat_quote = "Ini adalah contoh escape string untuk quote sebuah \"quote\""
+print(kalimat_quote)
+
+baris = "Baris pertama. \nBaris kedua"
+print(baris)
+
+print("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+
+print(3*'wiu'+'tiit')
+
