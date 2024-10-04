5d1608a001c4fdbeb8c65912babff2a6b1a4f899
Efend1 <kujanglarang31@gmail.com>
2024-10-04 19:39:19 +0700

    update latihan1

---
+++ latihan1/loops.py
@@ -0,0 +1,14 @@
+count = 0
+while (count < 5):
+    count = count + 1
+    print("Hello Fenn")
+
+
+
+count = 0
+while (count < 5):
+    count = count + 1
+    print("Hello Geek")
+else:
+    print("sudah melebihi kondisi while")
+
